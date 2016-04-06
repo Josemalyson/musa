b2d9f878828880651369d8729a04de6f34ab4359
josemalyson <josemalyson@gmail.com>
2016-04-06 13:45:39 -0300

    Corrigindo erro na chamada dos icons do bootstap

--- src/com/br/musa/controlador/UsuarioControlador.java
+++ src/com/br/musa/controlador/UsuarioControlador.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
 
+import com.br.musa.constantes.MsgConstantes;
 import com.br.musa.entidades.RegrasUsuario;
 import com.br.musa.entidades.Usuario;
 import com.br.musa.exeption.MusaExecao;
@@ -51,6 +52,7 @@ public class UsuarioControlador extends CoreControlador {
 	public void salvar(){
 		try {
 			usuarioServico.salvar(usuario);
+			adicionarMensagem(MsgConstantes.MSG_SUCESSO);
 		} catch (MusaExecao e) {
 			logger.error(e.getMessage(),e);
 			adicionarErro(e.getMessage());
